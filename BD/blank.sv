b81ee1cc8e5822564021521b55a4f7de01740785
Rafael Pontes <rafaelgpontes1@gmail.com>
2017-03-08 19:50:17 -0300

    Reorganizando arquivos.

---
+++ BD/blank.sv
@@ -0,0 +1,15 @@
+module Demodulador(
+  input logic G_CLK_RX,
+  input logic [7:0]ADC,
+  output logic int_rx_host,
+  output logic [7:0]data,
+  inout logic [7:0]BD_CONTROL
+);
+  
+  logic [7:0] contador = 0;
+
+always_ff @(G_CLK_RX) begin
+    
+end
+  
+endmodule
---
+++ BD/testbench.sv
@@ -0,0 +1,6 @@
+// Code your testbench here
+// or browse Examples
+module tb();
+  
+  
+endmodule
